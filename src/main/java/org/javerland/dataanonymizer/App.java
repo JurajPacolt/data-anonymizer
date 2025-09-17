183bfc7e803a5e10bd46e55d2d4f7b3b9d98d64c
juraj.pacolt <juraj.pacolt@assecosol.com>
2025-09-17 20:56:50 +0200

    initial commit

--- src/main/java/org/javerland/dataanonymizer/App.java
+++ src/main/java/org/javerland/dataanonymizer/App.java
@@ -1,12 +1,35 @@
 /* Created on 17.09.2025 */
 package org.javerland.dataanonymizer;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
 /**
  * @author Juraj Pacolt
  */
-public class App {
+//@formatter:off
+@Command(name = "data-anonymizer", mixinStandardHelpOptions = true, version = "0.0.1",
+        description = "Data anonymizer tool by JDBC")
+//@formatter:on
+public class App implements Runnable {
+
+    @Option(names = { "-d",
+            "--driver" }, description = "JDBC driver class", defaultValue = "org.postgresql.Driver", required = true)
+    private String driver = "org.postgresql.Driver";
+    @Option(names = { "-l", "--url" }, description = "URL connection string", required = true)
+    private String url = null;
+    @Option(names = { "-u", "--username" }, description = "User name", required = false)
+    private String username = null;
+    @Option(names = { "-p", "--password" }, description = "Password", required = false)
+    private String password = null;
+
+    @Override
+    public void run() {
+        // TODO ...
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.exit(new CommandLine(new App()).execute(args));
     }
 }
