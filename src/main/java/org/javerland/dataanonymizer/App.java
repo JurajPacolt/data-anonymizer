fa92278279f0cfd6236d5b97c17a23bd8099b222
juraj.pacolt <juraj.pacolt@assecosol.com>
2025-09-17 20:17:15 +0200

    initial commit

---
+++ src/main/java/org/javerland/dataanonymizer/App.java
@@ -0,0 +1,13 @@
+package org.javerland.dataanonymizer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
